c1d5f57213eb8bc83ef29fa645c995ebf080fd5b
Karan Singh <76433381+ksingh-scogo@users.noreply.github.com>
2023-12-06 21:50:31 +0530

    Update 03_tinyproxy.sh

--- 03_tinyproxy.sh
+++ 03_tinyproxy.sh
@@ -1,27 +1,3 @@
-#!/bin/sh
-
-START=10
-STOP=15
-
-start() {
-        echo "Starting TinyProxy"
-        touch /var/log/tinyproxy.log
-	chown nobody:nogroup /var/log/tinyproxy.log
-        /usr/bin/tinyproxy -d -c /etc/config/tinyproxy.conf &
-}
-
-stop() {
-        echo "Stopping TinyProxy"
-        killall tinyproxy
-}
-
-restart() {
-        echo "Restarting TinyProxy"
-        stop
-        start
-	return 0
-}
-
 touch /var/log/tinyproxy.log
 chown nobody:nogroup /var/log/tinyproxy.log
 /usr/bin/tinyproxy -d -c /etc/config/tinyproxy.conf &
